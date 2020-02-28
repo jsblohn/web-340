e9a9e1c386166435f6fe4871e5e744a38e5da48c
Janet Blohn <jblohn@my365.bellevue.edu>
2020-02-28 09:07:23 -0600

    Add Exercise 2.2

---
+++ week-2/hello-world/blohn-exercise-2.2.js
@@ -0,0 +1,29 @@
+/*
+============================================
+; Title:  blohn-exercise-2.2.js
+; Author: Professor Krasso
+; Date:   27 February 2020
+; Modified By: Janet Blohn
+; Description: Exercise 2.2 - Hello World with Express
+; Create the Hello World JavaScript program and run using Express.
+============================================
+*/
+
+// Import blohn-header.js file
+const header = require('../../blohn-header');
+
+// Print the Header
+console.log(header.display("Janet", "Blohn", "Exercise 2.2"));
+
+// Create the variables.
+var express = require("express");
+var http = require("http");
+
+var app = express();
+
+app.use(function(request, response) {
+  console.log("In comes a request to: " + request.url);
+  response.end("Hello World");
+});
+
+http.createServer(app).listen(8080);
